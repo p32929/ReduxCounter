dc1509ff08f6ff91811ff13bf2339c9d1d912d90
p32929 <p32929@yahoo.com>
2021-09-03 16:33:46 +0600

    just updated the dependencies

--- src/redux/Reducer.js
+++ src/redux/Reducer.js
@@ -1,32 +1,29 @@
-import {createStore} from 'redux'
+import { createStore } from 'redux'
 
 var store = {
     count: 0
 }
 
-function reducer(state = store, action) {
+function reducer_fn(state = store, action) {
     switch (action.type) {
         case 'INCREMENT':
             return {
                 count: state.count + 1
             }
-            break;
 
         case 'DECREMENT':
             return {
                 count: state.count - 1
             }
-            break;
 
         case 'RESET':
             return {
                 count: 0
             }
-            break;
 
         default:
             return state
     }
 }
 
-export const reducer = createStore(reducer)
+export const reducer = createStore(reducer_fn)
